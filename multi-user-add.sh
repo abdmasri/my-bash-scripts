7e99c39ffd2db0f0e12230fa67f62ae2ee5a7a75
abdmasri <abd.masri993@gmail.com>
2021-06-26 19:37:31 +0800

    Update multi-user-add.sh

--- multi-user-add.sh
+++ multi-user-add.sh
@@ -3,26 +3,31 @@
 ## This script will creat multiple users with random generated psasword (16 char) via randpw function.
 
 randpw() { < /dev/urandom tr -dc _'12345!@#$%qwertQWERTasdfgASDFGzxcvbZXCVB'|  head -c${1:-16};echo;}
-date=date +"%m.%d.%Y"
+date_var=$(date +"%m.%d.%Y")
+time_var=$(date +"%T")
 
+echo "Begin $time_var" >> /tmp/muser-log-$date_var.log
 ## Read the users from first Parameter (must be file) 
 for i in $( cat $1) ;
-    do 
+do 
+newpw=$(randpw)
 
-    newpw=$(randpw)
+    if [[ "${UID}" -eq 0 ]]
+        then   
 
-    useradd "${i}" &>> /tmp/muser-log-$date.log 
-
-    if [ $? -eq 0 ]
-
-      then :    # do nothing and go to line number 24#
-
-      else echo "User $i already exist "
-        continue
+         useradd "${i}" &>> /tmp/muser-log-$date_var.log 
+        else 
+         echo "you require root privilege"
+         exit 1 
+    fi
+    if [ $? -ne 0 ]
+      then 
+       echo "User $i already exist -- check error logs"
+       continue
     fi
 
-echo "${newpw}" |passwd  --stdin "${i}"  > /dev/null
-
-
+echo "${newpw}" |passwd  --stdin "${i}"  >> /tmp/muser-log-$date_var.log 
 echo -e  "password for $i: $newpw"
+
 done
+echo -e  "END $time_var\n\n" >> /tmp/muser-log-$date_var.log
